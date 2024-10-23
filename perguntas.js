8f4ab9016e5b43f6b7571f6df02c9082aa070905
snowzssad <168860478+snowzssad@users.noreply.github.com>
2024-10-23 13:10:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Conceito',
+    'O que é Days Gone?',
+    'Days Gone é um jogo de ação e sobrevivência em mundo aberto desenvolvido pela Bend Studio e lançado pela Sony Interactive Entertainment em 2019. O jogo se passa em um mundo pós-apocalíptico infestado por zumbis, chamados de "freakers".'
+)
+
+criaCartao(
+    'Ambiente',
+    'Qual é a ambientação de Days Gone?',
+    'O jogo se passa em um cenário pós-apocalíptico no estado de Oregon, Estados Unidos, onde a sociedade foi devastada por uma pandemia que transformou humanos em criaturas agressivas. O jogador explora florestas, montanhas e pequenas cidades abandonadas.'
+)
+
+criaCartao(
+    'Protagonista',
+    'Quem é o protagonista do jogo?',
+    'O protagonista de Days Gone é Deacon St. John, um ex-membro de gangue de motoqueiros que agora trabalha como mercenário e caçador de recompensas, enquanto busca sobrevivência e respostas sobre o paradeiro de sua esposa, Sarah.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como a mecânica de sobrevivência e combate afeta a experiência do jogador em Days Gone?',
+    'A mecânica de sobrevivência de Days Gone é um dos principais fatores que torna o jogo desafiador e imersivo. O jogador precisa gerenciar recursos limitados, como munição, combustível para a moto e suprimentos médicos, o que força decisões estratégicas durante a exploração do vasto mundo aberto. A moto de Deacon é essencial tanto para a mobilidade quanto para a sobrevivência, já que ela serve como meio de transporte e armazenamento, mas também requer manutenção e combustível constante. Além disso, o sistema de combate permite abordagens variadas, desde combate direto contra hordas de freakers até furtividade para evitar confrontos. Esses elementos de sobrevivência somam uma camada de tensão ao gameplay, já que os encontros com inimigos podem ser letais, especialmente em áreas infestadas. A interação com o ambiente e o uso de táticas adequadas tornam cada decisão no jogo significativa e impactam diretamente a narrativa e o progresso.'
+
+
+
+)
